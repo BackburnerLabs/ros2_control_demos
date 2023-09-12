03a97e593c749e87e92fdc74ded806724dfdf2d9
Michael Sandstedt <mike@backburnerlabs.io>
2023-09-11 21:50:57 -0500

    Pin controller_manager for example_2 to core 3

--- example_2/bringup/launch/diffbot.launch.py
+++ example_2/bringup/launch/diffbot.launch.py
@@ -64,6 +64,7 @@ def generate_launch_description():
         executable="ros2_control_node",
         parameters=[robot_description, robot_controllers],
         output="both",
+        prefix=["taskset -c 3"],
     )
     robot_state_pub_node = Node(
         package="robot_state_publisher",
